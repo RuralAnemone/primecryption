5bcf48828bfc2008ddc00be55413c5d75641c137
Rural Anemone <dev@ruralanemone.tech>
2023-02-07 16:28:29 -0600

    don't shuffle and therefore no index

--- encrypt.js
+++ encrypt.js
@@ -6,16 +6,7 @@ function encrypt(string) {
         const asciiValue = string.charCodeAt(i);
         array.push(nthPrime(asciiValue) * nthPrime(i));
     });
-    randomizeArray(array);
     return array.join("|");
-}
-
-function randomizeArray(array) {
-    array = array
-        .map((value) => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value);
-    // stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/46545530#46545530
 }
 
 export default encrypt;
