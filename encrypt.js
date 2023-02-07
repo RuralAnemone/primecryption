db6ef24728204d8c4d3cd3456861665a0f9d8401
Rural Anemone <dev@ruralanemone.tech>
2023-02-07 16:29:04 -0600

    oopsies forgot to save the file :)

--- encrypt.js
+++ encrypt.js
@@ -4,7 +4,7 @@ function encrypt(string) {
     let array = [];
     [...string].forEach((char, i) => { // plop string into an array of characters
         const asciiValue = string.charCodeAt(i);
-        array.push(nthPrime(asciiValue) * nthPrime(i));
+        array.push(nthPrime(asciiValue));
     });
     return array.join("|");
 }
